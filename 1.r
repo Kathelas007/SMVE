ad98ed91e66b7c9956d49606a9736d903569e205
KaterinaMuskova <44769003+Kathelas007@users.noreply.github.com>
2023-07-03 13:32:12 +0000

    Add files via upload

---
+++ 1.r
@@ -0,0 +1,49 @@
+
+
+
+
+
+
+
+# vektory
+
+
+
+
+
+# pole a matice
+
+# datov tabulka
+# typ studia, vek, body, bydliste (I/P)
+
+
+# pridani sloupce
+
+
+
+
+
+# vyukove data, MASS
+
+
+
+
+
+# auta bez airbagou
+c <- subset(Cars93, Cars93$AirBags == FALSE)
+c
+
+# basic grafy
+plot(cos, from = -2 * pi, to = pi, xlab = "os x", ylab = "cos x")
+par(mfrow = c(1, 2))
+
+
+
+# kolacove a sloupcove
+
+
+# DU import excel dat
+# misto odevzdani, staci html soubor (View)
+
+
+
